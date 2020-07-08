c979ccebd8ed0bf788912e474f50b8c5859abf36
zlxxwhu@gmail.com <550631696@qq.com>
2020-07-08 10:27:30 +0800

    test

---
+++ sosotaxi/sosotext-service/src/main/java/com/apicaller/sosotaxi/BootstrapMain.java
@@ -0,0 +1,14 @@
+package com.apicaller.sosotaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Beingwild
+ */
+@SpringBootApplication
+public class BootstrapMain {
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapMain.class,args);
+    }
+}
