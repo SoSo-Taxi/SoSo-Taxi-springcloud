1cc9aad1e8c565b6e495df4e5d0d45d19e11605f
LUO12826 <luohuizhou@hotmail.com>
2020-07-13 17:44:33 +0800

    L-修改实体类。

---
+++ sosotaxi/common/src/main/java/com/apicaller/sosotaxi/entity/GeoPoint.java
@@ -0,0 +1,36 @@
+package com.apicaller.sosotaxi.entity;
+
+import lombok.Data;
+import java.io.Serializable;
+
+/**
+ * 表示一个地理上的点。
+ *
+ * @author 骆荟州
+ * @CreateTime 2020/7/13 13:11
+ * @UpdateTime
+ */
+@Data
+public class GeoPoint implements Serializable {
+    private static final long serialVersionUID = 42345441L;
+
+    public GeoPoint(double lat, double lng) {
+        this.lng = lng;
+        this.lat = lat;
+    }
+
+    /** 纬度 */
+    private double lat;
+
+    /** 经度 */
+    private double lng;
+
+    @Override
+    public String toString(){
+        return "Point(lat:" + lat + ", lng:" + lng + ")";
+    }
+
+    public String toPointFormat() {
+        return "POINT(" + lat + " " + lng + ")";
+    }
+}
