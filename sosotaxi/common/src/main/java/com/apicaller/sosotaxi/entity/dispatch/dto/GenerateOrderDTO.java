90a9fd4e48778bcc5c30b1c0ba5336dd7098af54
LUO12826 <luohuizhou@hotmail.com>
2020-07-16 15:41:53 +0800

    L-增加一些数据传输类。

---
+++ sosotaxi/common/src/main/java/com/apicaller/sosotaxi/entity/dispatch/dto/GenerateOrderDTO.java
@@ -0,0 +1,34 @@
+package com.apicaller.sosotaxi.entity.dispatch.dto;
+
+import com.apicaller.sosotaxi.entity.GeoPoint;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 前端发起打车请求时通过Json传来到数据。
+ *
+ * @author: 骆荟州
+ * @createTime: 2020/7/16 2:54 下午
+ * @updateTime:
+ */
+@Data
+public class GenerateOrderDTO implements Serializable {
+    private static final long serialVersionUID = 4484485641L;
+
+    private String city;
+
+    private Long passengerId;
+
+    /**考虑到可能有预约订单，这里命名为出发时间。*/
+    private Date departTime;
+
+    private Short passengerNum;
+
+    private GeoPoint departPoint;
+
+    private GeoPoint destPoint;
+
+    private Short serviceType;
+}
