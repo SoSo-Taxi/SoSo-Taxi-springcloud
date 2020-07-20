80d0dd08d9c199202dcfecd11e9a094bcc96b3b9
LUO12826 <luohuizhou@hotmail.com>
2020-07-20 09:44:56 +0800

    L-鹰眼工具类和百度地图工具类的修改。

--- sosotaxi/common/src/main/java/com/apicaller/sosotaxi/utils/BDmapUtil.java
+++ sosotaxi/common/src/main/java/com/apicaller/sosotaxi/utils/BDmapUtil.java
@@ -1,11 +1,20 @@
 package com.apicaller.sosotaxi.utils;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.apicaller.sosotaxi.entity.GeoPoint;
+import com.apicaller.sosotaxi.entity.bdmap.SimpleDistance;
+import com.apicaller.sosotaxi.entity.bdmap.SimplePosition;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.*;
 
 /**
  * 调用百度地图WebApi的工具类
+ * 此类中的方法有：
+ * 1 逆地理编码
+ * 2 批量算路
+ *
  * @author: 骆荟州
  * @createTime: 2020/7/17 6:08 下午
  * @updateTime:
@@ -58,5 +67,88 @@ public class BDmapUtil {
                 .retrieve().bodyToMono(String.class).block();
 
         return JSONObject.parseObject(res);
+    }
+
+    /**
+     * 调用批量算路Api。计算多个点到多个点的距离
+     * 结果顺序是：
+     * origin1 -- dest1
+     * origin1 -- dest2
+     * origin2 -- dest1
+     * origin2 -- dest2
+     */
+    public static <T> List<SimpleDistance<T>> batchCalcDistance(List<SimplePosition<T>> origins,
+                                                                CoordType coordType, List<SimplePosition<T>> dest) {
+        if(origins == null || dest == null) {
+            return null;
+        }
+        //适应百度起点终点之乘积不能超过50的要求
+        if(dest.size() * origins.size() > 50) {
+            dest = dest.subList(0, 50 / origins.size());
+            if(dest.isEmpty()) {
+                return null;
+            }
+        }
+        coordType = coordType == null ? CoordType.bd09ll : coordType;
+
+        //调用百度地图API查询每对可能的乘客司机对之间的距离。
+        CoordType finalCoordType = coordType;
+        List<SimplePosition<T>> finalDest = dest;
+        String res = WebClient.create()
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("http")
+                        .host("api.map.baidu.com")
+                        .path("/routematrix/v2/driving")
+                        .queryParam("ak", AK)
+                        .queryParam("coord_type", finalCoordType.toString())
+                        .queryParam("tactics", "11")
+                        .queryParam("origins", buildPointString(origins))
+                        .queryParam("destinations", buildPointString(finalDest))
+                        .build())
+                .retrieve().bodyToMono(String.class).block();
+
+        JSONObject jsonObject = JSONObject.parseObject(res);
+        if(jsonObject.getInteger("status") != 0) {
+            return null;
+        }
+
+        List<SimpleDistance<T>> resultList = new ArrayList<SimpleDistance<T>>();
+        JSONArray resultArr = jsonObject.getJSONArray("result");
+        int originCount = origins.size();
+        int count = resultArr.size();
+        for(int i = 0; i < count; i++) {
+            double duration = JSONObject.parseObject(resultArr.get(i).toString())
+                                        .getJSONObject("duration").getDouble("value");
+
+            resultList.add(new SimpleDistance<T>(origins.get(count/originCount).getTag(),
+                    dest.get(count%originCount).getTag(), duration));
+        }
+        return resultList;
+    }
+
+    /**
+     * 调用批量算路Api。此方法计算一个点到至多50个点的距离。
+     */
+    public static <T> List<SimpleDistance<T>> batchCalcDistance(SimplePosition<T> origin,
+                                                                CoordType coordType, List<SimplePosition<T>> dest) {
+        List<SimplePosition<T>> list = new ArrayList<SimplePosition<T>>();
+        list.add(origin);
+        return batchCalcDistance(list, coordType, dest);
+    }
+
+    /** 将一个个的点转为百度地图Api调用时需要的字符串格式
+     * 点与点之间用"｜"分隔。
+     */
+    private static <T> String buildPointString(List<SimplePosition<T>> pos) {
+        StringBuilder output = new StringBuilder("");
+        int count = pos.size();
+        for(int i = 0; i < count; i++) {
+            if(i != 0) {
+                output.append("|");
+            }
+            output.append(pos.get(i).getPoint().toBDFormat());
+        }
+        return output.toString();
     }
 }
